23873a86965d4c0edd465a55c722e217147366f3
Douglas Ray <dpr980@gmail.com>
2019-05-06 15:28:03 +0100

    added api endpoint

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + 'index.html'))
+
+app.post('/api/fileanalyse', (req, res) => {
+	// I can submit a form that includes a file upload.
+	// The form file input field has the "name" attribute set to "upfile". We rely on this in testing.
+	// When I submit something, I will receive the file name and size in bytes within the JSON response
+	res.json({ filename: '', size: '' })
+})
+
+app.listen(process.env.PORT || 4000, () => {
+	console.log('Your app is listening on port 4000')
+})
